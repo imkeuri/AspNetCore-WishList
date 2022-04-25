705703049eaadcee7670a5c394c7c825f3dc1e3e
Keuri <kcastillo@evopointsolution.com>
2022-04-25 12:54:26 -0400

    x

--- WishList/Controllers/ItemController.cs
+++ WishList/Controllers/ItemController.cs
@@ -40,9 +40,9 @@ namespace WishList.Controllers
         [HttpDelete]
         public IActionResult Delete(int Id)
         {
-            var itemToDelete = _context.Items.FirstOrDefault(x => x.Id == Id);
+            var item = _context.Items.FirstOrDefault(e => e.Id == Id);
 
-            _context.Items.Remove(itemToDelete);
+            _context.Items.Remove(item);
             _context.SaveChanges();
 
             return RedirectToAction("Index");
